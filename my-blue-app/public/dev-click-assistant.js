8ecd3620f2859ce742c95287cd1e7ea5df5d0427
shivareddy <shiva.r547@gmail.com>
2025-10-18 14:28:36 +0530

    all working with from react fronend will do development scrtpt

--- my-blue-app/public/dev-click-assistant.js
+++ my-blue-app/public/dev-click-assistant.js
@@ -2,9 +2,9 @@
     'use strict';
 
     // Only run on localhost
-    if (!["localhost", "127.0.0.1", "0.0.0.0"].includes(window.location.hostname)) {
-        return;
-    }
+    // if (!["localhost", "127.0.0.1", "0.0.0.0"].includes(window.location.hostname)) {
+    //     return;
+    // }
 
     console.log("🧠 Dev Assistant Loading...");
 
